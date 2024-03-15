654eeca5e75ffa5cfe91ae28f7cf2ad6e39629f3
Wm. Weatherspoon <binaryjohn@gmail.com>
2024-03-14 22:51:06 -0700

    chore: add fugitive

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -12,5 +12,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
   config = function()
     require('gitsigns').setup()
     vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+    vim.keymap.set('n', '<leader>B', ':Gitsigns toggle_current_line_blame<CR>', {})
   end,
 }
