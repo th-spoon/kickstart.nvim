4b368cd9283fd255dd19ff5a212a4619efd42836
Wm. Weatherspoon <binaryjohn@gmail.com>
2024-03-14 20:23:38 -0700

    refactor: gitsigns to plugin file

--- init.lua
+++ init.lua
@@ -154,6 +154,7 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+vim.keymap.set('n', '<leader>d', ':Neotree toggle  reveal_force_cwd<cr>')
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -1,12 +1,16 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  -- opts = {
+  --   signs = {
+  --     add = { text = '+' },
+  --     change = { text = '~' },
+  --     delete = { text = '_' },
+  --     topdelete = { text = '‾' },
+  --     changedelete = { text = '~' },
+  --   },
+  -- },
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', {})
+  end,
 }
